57fe92bba8c44eb90eb44d9533653424f380c9c8
jishnup89 <jishnup89@gmail.com>
2018-09-24 18:19:26 +0530

    changed

---
+++ WebSiteTest/src/main/java/WebSiteTest/WebSiteTest/App.java
@@ -0,0 +1,13 @@
+package WebSiteTest.WebSiteTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ WebSiteTest/src/test/java/example/Newtest.java
@@ -0,0 +1,69 @@
+package example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.google.common.base.Verify;
+
+public class Newtest {
+	public WebDriver driver ;
+	@BeforeTest
+	public void init() throws InterruptedException
+	{
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\dell\\Downloads\\geckodriver.exe" );
+		 driver=new FirefoxDriver();
+		//Thread.sleep(3000);
+		
+		
+	}
+	@Test
+	public void test() throws InterruptedException
+	{
+		String expected ="SEM Webpage";
+		driver.get("http://18.217.126.56:8080/hello/");
+		String actual=driver.findElement(By.xpath("/html/body/table/tbody/tr/td[2]/h1")).getText();
+	if(actual.contentEquals(expected))
+		{
+			System.out.println("SEM WEBPAGE exsist");
+		}
+		else
+		{
+			System.out.println("SEM WEBPAGE does not exsist");
+		}
+	       WebElement li=driver.findElement(By.xpath("/html/body/ul/li/a"));
+	       if(li.isDisplayed() && li.isEnabled())
+	       {
+	    	   li.click();
+	    	   Thread.sleep(2000);
+//	    	  Boolean bo=driver.findElement(By.xpath("/html/body/div[2]/header/div/div/div/div[1]/a/img")).isDisplayed();
+//	          if(bo.toString()=="True")
+//	          {
+//	        	  System.out.println("Image exist");
+//	          }
+//	          else
+//	          {
+//	        	  System.out.println("Image does not exist");
+//	          }
+      }
+	       
+	} 
+	       @AfterTest
+	       public void aft()
+	       {
+	    	   driver.quit();
+	    	   driver.close();
+	       }
+	       
+	
+	
+	
+	
+
+}
