6318b9027350585d9a700eb58d272f8d80c03c05
jkrishna859 <jkrishna859@gmail.com>
2018-11-27 16:49:02 +0530

    New test

---
+++ WebDriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,34 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	public WebDriver driver ;
+	@Test
+	public void test() throws InterruptedException
+	{
+//		String expected ="SEM Webpage";
+		driver.get("http://18.223.101.200:8080/hello/");
+		driver.findElement(By.linkText("Se-Mentor")).click();
+	}	
+  @BeforeTest
+	public void init() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Downloads\\chromedriver\\chromedriver.exe");
+		 driver=new ChromeDriver();
+		Thread.sleep(3000);
+	}
+
+  @AfterTest
+  public void aft()
+  {
+	   //driver.quit();
+	   driver.close();
+  }
+
+}
